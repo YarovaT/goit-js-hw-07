9a16c7eae7065561f20098e67682ed5cc07d21a9
Tetiana Yarova <andronikova11@gmail.com>
2021-04-24 12:39:09 +0300

    Решает задачу №5

---
+++ src/js/task-05.js
@@ -0,0 +1,16 @@
+/*Напиши скрипт который, при наборе текста в инпуте `input#name-input` (событие
+input), подставляет его текущее значение в `span#name-output`. Если инпут
+пустой, в спане должна отображаться строка `'незнакомец'`.*/
+
+const inputEvent = document.querySelector('#name-input');
+console.dir(inputEvent);
+const nameInput = document.querySelector('#name-output');
+console.log(nameInput.textContent);
+
+inputEvent.addEventListener('input', onInputSubmit);
+
+function onInputSubmit(name) {
+    if (name = inputEvent.value) {
+        nameInput.textContent = name;
+   }
+}
